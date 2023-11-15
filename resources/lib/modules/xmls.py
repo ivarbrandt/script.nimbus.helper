f5227b6a2414fd758408d9713d085b1ea7001e3d
Ivar Brandt <ibrandt2@gmail.com>
2023-11-14 19:55:15 -0700

    flipped stacked widget back on top

--- resources/lib/modules/xmls.py
+++ resources/lib/modules/xmls.py
@@ -25,19 +25,18 @@ history_xml_body = "\
 
 
 stacked_media_xml_body = '\
-\n        <include content="{cpath_type}">\
-\n            <param name="content_path" value="$INFO[Window(Home).Property(nimbus.{cpath_list_id}.path)]"/>\
-\n            <param name="widget_header" value="$INFO[Window(Home).Property(nimbus.{cpath_list_id}.label)]"/>\
-\n            <param name="widget_target" value="videos"/>\
-\n            <param name="list_id" value="{cpath_list_id}1"/>\
-\n        </include>\
 \n        <include content="WidgetListCategoryStacked">\
 \n            <param name="content_path" value="{cpath_path}"/>\
 \n            <param name="widget_header" value="{cpath_header}"/>\
 \n            <param name="widget_target" value="videos"/>\
 \n            <param name="list_id" value="{cpath_list_id}"/>\
+\n        </include>\
+\n        <include content="{cpath_type}">\
+\n            <param name="content_path" value="$INFO[Window(Home).Property(nimbus.{cpath_list_id}.path)]"/>\
+\n            <param name="widget_header" value="$INFO[Window(Home).Property(nimbus.{cpath_list_id}.label)]"/>\
+\n            <param name="widget_target" value="videos"/>\
+\n            <param name="list_id" value="{cpath_list_id}1"/>\
 \n        </include>'
-
 
 main_menu_movies_xml = '\
 <?xml version="1.0" encoding="UTF-8"?>\
