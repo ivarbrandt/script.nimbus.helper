f8241fa7a7ae388a9816603c2671f9f5f190debf
Ivar Brandt <ibrandt2@gmail.com>
2023-11-26 17:46:00 -0700

    bug fix - removed starting_widgets

--- resources/lib/modules/version_monitor.py
+++ resources/lib/modules/version_monitor.py
@@ -23,14 +23,13 @@ def check_for_update(skin_id):
         return set_installed_version(skin_id, installed_version)
     if property_version == installed_version:
         return
-    from modules.cpath_maker import remake_all_cpaths, starting_widgets
+    from modules.cpath_maker import remake_all_cpaths
     from modules.search_utils import remake_all_spaths
 
     set_installed_version(skin_id, installed_version)
     sleep(1000)
     remake_all_cpaths(silent=True)
     remake_all_spaths(silent=True)
-    starting_widgets()
 
 
 def set_installed_version(skin_id, installed_version):
