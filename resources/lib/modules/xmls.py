2691895e51c9e74f5d7e294085cd6bb8976d38c1
Ivar Brandt <ibrandt2@gmail.com>
2024-06-24 06:52:39 -0600

    Restructured XML blocks

--- resources/lib/modules/xmls.py
+++ resources/lib/modules/xmls.py
@@ -1,126 +1,116 @@
 # -*- coding: utf-8 -*-
 
-media_xml_start = '\
-<?xml version="1.0" encoding="UTF-8"?>\
-\n<includes>\
-\n    <include name="{main_include}">'
+media_xml_start = '''<?xml version="1.0" encoding="UTF-8"?>
+<includes>
+  <include name="{main_include}">'''
 
-media_xml_end = "\
-\n    </include>\
-\n</includes>"
+media_xml_end = '''
+  </include>
+</includes>'''
 
-media_xml_body = '\
-\n        <include content="{cpath_type}">\
-\n            <param name="content_path" value="{cpath_path}"/>\
-\n            <param name="widget_header" value="{cpath_header}"/>\
-\n            <param name="widget_target" value="videos"/>\
-\n            <param name="list_id" value="{cpath_list_id}"/>\
-\n        </include>'
+media_xml_body = '''
+    <include content="{cpath_type}">
+      <param name="content_path" value="{cpath_path}"/>
+      <param name="widget_header" value="{cpath_header}"/>
+      <param name="widget_target" value="videos"/>
+      <param name="list_id" value="{cpath_list_id}"/>
+    </include>'''
 
-history_xml_body = "\
-\n        <item>\
-\n            <label>$NUMBER[{spath}]</label>\
-\n            <onclick>RunScript(script.nimbus.helper,mode=re_search)</onclick>\
-\n        </item>"
+history_xml_body = '''
+    <item>
+      <label>$NUMBER[{spath}]</label>
+      <onclick>RunScript(script.nimbus.helper,mode=re_search)</onclick>
+    </item>'''
 
-main_menu_movies_xml = '\
-<?xml version="1.0" encoding="UTF-8"?>\
-\n<includes>\
-\n    <include name="MoviesMainMenu">\
-\n        <item>\
-\n            <label>{cpath_header}</label>\
-\n            <onclick>ActivateWindow(Videos,{main_menu_path},return)</onclick>\
-\n            <property name="menu_id">$NUMBER[19000]</property>\
-\n            <property name="id">movies</property>\
-\n            <visible>!Skin.HasSetting(HomeMenuNoMovieButton)</visible>\
-\n        </item>\
-\n    </include>\
-\n</includes>'
+main_menu_movies_xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<includes>
+  <include name="MoviesMainMenu">
+    <item>
+      <label>{cpath_header}</label>
+      <onclick>ActivateWindow(Videos,{main_menu_path},return)</onclick>
+      <property name="menu_id">$NUMBER[19000]</property>
+      <property name="id">movies</property>
+      <visible>!Skin.HasSetting(HomeMenuNoMovieButton)</visible>
+    </item>
+  </include>
+</includes>'''
 
-main_menu_tvshows_xml = '\
-<?xml version="1.0" encoding="UTF-8"?>\
-\n<includes>\
-\n    <include name="TVShowsMainMenu">\
-\n        <item>\
-\n            <label>{cpath_header}</label>\
-\n            <onclick>ActivateWindow(Videos,{main_menu_path},return)</onclick>\
-\n            <property name="menu_id">$NUMBER[22000]</property>\
-\n            <property name="id">tvshows</property>\
-\n            <visible>!Skin.HasSetting(HomeMenuNoTVShowButton)</visible>\
-\n        </item>\
-\n    </include>\
-\n</includes>'
+main_menu_tvshows_xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<includes>
+  <include name="TVShowsMainMenu">
+    <item>
+      <label>{cpath_header}</label>
+      <onclick>ActivateWindow(Videos,{main_menu_path},return)</onclick>
+      <property name="menu_id">$NUMBER[22000]</property>
+      <property name="id">tvshows</property>
+      <visible>!Skin.HasSetting(HomeMenuNoTVShowButton)</visible>
+    </item>
+  </include>
+</includes>'''
 
-main_menu_custom1_xml = '\
-<?xml version="1.0" encoding="UTF-8"?>\
-\n<includes>\
-\n    <include name="Custom1MainMenu">\
-\n        <item>\
-\n            <label>{cpath_header}</label>\
-\n            <onclick>ActivateWindow(Videos,{main_menu_path},return)</onclick>\
-\n            <property name="menu_id">$NUMBER[23000]</property>\
-\n            <property name="id">custom1</property>\
-\n            <visible>!Skin.HasSetting(HomeMenuNoCustom1Button)</visible>\
-\n        </item>\
-\n    </include>\
-\n</includes>'
+main_menu_custom1_xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<includes>
+  <include name="Custom1MainMenu">
+    <item>
+      <label>{cpath_header}</label>
+      <onclick>ActivateWindow(Videos,{main_menu_path},return)</onclick>
+      <property name="menu_id">$NUMBER[23000]</property>
+      <property name="id">custom1</property>
+      <visible>!Skin.HasSetting(HomeMenuNoCustom1Button)</visible>
+    </item>
+  </include>
+</includes>'''
 
-main_menu_custom2_xml = '\
-<?xml version="1.0" encoding="UTF-8"?>\
-\n<includes>\
-\n    <include name="Custom2MainMenu">\
-\n        <item>\
-\n            <label>{cpath_header}</label>\
-\n            <onclick>ActivateWindow(Videos,{main_menu_path},return)</onclick>\
-\n            <property name="menu_id">$NUMBER[24000]</property>\
-\n            <property name="id">custom2</property>\
-\n            <visible>!Skin.HasSetting(HomeMenuNoCustom2Button)</visible>\
-\n        </item>\
-\n    </include>\
-\n</includes>'
+main_menu_custom2_xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<includes>
+  <include name="Custom2MainMenu">
+    <item>
+      <label>{cpath_header}</label>
+      <onclick>ActivateWindow(Videos,{main_menu_path},return)</onclick>
+      <property name="menu_id">$NUMBER[24000]</property>
+      <property name="id">custom2</property>
+      <visible>!Skin.HasSetting(HomeMenuNoCustom2Button)</visible>
+    </item>
+  </include>
+</includes>'''
 
-main_menu_custom3_xml = '\
-<?xml version="1.0" encoding="UTF-8"?>\
-\n<includes>\
-\n    <include name="Custom3MainMenu">\
-\n        <item>\
-\n            <label>{cpath_header}</label>\
-\n            <onclick>ActivateWindow(Videos,{main_menu_path},return)</onclick>\
-\n            <property name="menu_id">$NUMBER[25000]</property>\
-\n            <property name="id">custom3</property>\
-\n            <visible>!Skin.HasSetting(HomeMenuNoCustom3Button)</visible>\
-\n        </item>\
-\n    </include>\
-\n</includes>'
+main_menu_custom3_xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<includes>
+  <include name="Custom3MainMenu">
+    <item>
+      <label>{cpath_header}</label>
+      <onclick>ActivateWindow(Videos,{main_menu_path},return)</onclick>
+      <property name="menu_id">$NUMBER[25000]</property>
+      <property name="id">custom3</property>
+      <visible>!Skin.HasSetting(HomeMenuNoCustom3Button)</visible>
+    </item>
+  </include>
+</includes>'''
 
-search_history_xml = '\
-<?xml version="1.0" encoding="UTF-8"?>\
-\n<includes>\
-\n    <include name="SearchHistory">\
-\n        <item>\
-\n            <label>{spath}</label>\
-\n            <onclick>RunScript(script.nimbus.helper,mode=re_search)</onclick>\
-\n        </item>\
-\n    </include>\
-\n</includes>'
+search_history_xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<includes>
+  <include name="SearchHistory">
+    <item>
+      <label>{spath}</label>
+      <onclick>RunScript(script.nimbus.helper,mode=re_search)</onclick>
+    </item>
+  </include>
+</includes>'''
 
-default_widget = '\
-<?xml version="1.0" encoding="UTF-8"?>\
-\n<includes>\
-\n    <include name="{includes_type}">\
-\n    </include>\
-\n</includes>'
+default_widget = '''<?xml version="1.0" encoding="UTF-8"?>
+<includes>
+  <include name="{includes_type}">
+  </include>
+</includes>'''
 
-default_main_menu = '\
-<?xml version="1.0" encoding="UTF-8"?>\
-\n<includes>\
-\n    <include name="{includes_type}">\
-\n    </include>\
-\n</includes>'
+default_main_menu = '''<?xml version="1.0" encoding="UTF-8"?>
+<includes>
+  <include name="{includes_type}">
+  </include>
+</includes>'''
 
-default_history = '\
-<?xml version="1.0" encoding="UTF-8"?>\
-\n<includes>\
-\n    <include name="{includes_type}">\
-\n    </include>\
-\n</includes>'
+default_history = '''<?xml version="1.0" encoding="UTF-8"?>
+<includes>
+  <include name="{includes_type}">
+  </include>
+</includes>'''
